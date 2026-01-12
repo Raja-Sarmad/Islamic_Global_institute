75dc9aaf01d328f0d57473eb1ec7a0d8a544219d
CodeWithSarmad <rajasarmadaz@gmail.com>
2026-01-12 17:52:12 +0500

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import About from './Pages/AboutUs';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import Courses from './Pages/Courses';
